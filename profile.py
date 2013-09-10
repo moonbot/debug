61b4201c659c223be929c5e610acf1493fd9de3c
Brennan Chapman <brennan.chapman@gmail.com>
2013-09-10 09:13:02 -0500

    Added cacheGrind

--- profile.py
+++ profile.py
@@ -32,6 +32,7 @@ DOT_EXEC = dict(
 
 __all__  = [ 
     'dotMap',
+    'cacheGrind',
     'timeIt',
 ]
 
@@ -44,6 +45,16 @@ def dotMap(*dot_args, **dot_kwargs):
                 global _profile_result
                 _profile_result = func(*args, **kwargs)
             return createDotMap("resultWrapper()", *dot_args, **dot_kwargs)
+        return wrapper
+    return decorator
+
+def cacheGrind(*dot_args, **dot_kwargs):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            def resultWrapper():
+                global _profile_result
+                _profile_result = func(*args, **kwargs)
+            return createCacheGrind("resultWrapper()", *dot_args, **dot_kwargs)
         return wrapper
     return decorator
 
@@ -124,6 +135,7 @@ def createDotMap(cmd, outputImage=None, openImage=True, outputProfile=None, dotE
 
     # Create the gprof configuration
     gProfOutputPath = "{0}.gprofdot".format(os.path.splitext(outputImagePath)[0])
+    print "gProfOutputPath: {0}".format(gProfOutputPath) # TESTING
     if showStack:
         import traceback
         label = "\"{0}\"".format("\n".join(traceback.format_stack()))
@@ -138,17 +150,76 @@ def createDotMap(cmd, outputImage=None, openImage=True, outputProfile=None, dotE
         time.sleep(.1) # Wait for the image to be closed
         envtools.open_file(outputImagePath)
 
-    # Cleanup
-    try:
-        os.remove(gProfOutputPath)
-        # LOG.debug("Removed: {0}".format(gProfOutputPath))
-        if not saveOutputProfile:
-            os.remove(outputProfilePath)
-            # LOG.debug("Removed: {0}".format(outputProfilePath))
-    except Exception, e:
-        LOG.warning("Warning unable to remove profile. {0}".format(e))
+    # # Cleanup
+    # try:
+    #     # os.remove(gProfOutputPath)
+    #     # LOG.debug("Removed: {0}".format(gProfOutputPath))
+    #     if not saveOutputProfile:
+    #         os.remove(outputProfilePath)
+    #         # LOG.debug("Removed: {0}".format(outputProfilePath))
+    # except Exception, e:
+    #     LOG.warning("Warning unable to remove profile. {0}".format(e))
 
     return result
+
+def createCacheGrind(cmd, outputProfile=None, _frameDepth=1, **kwargs):
+    '''
+    Profile the execution of a command and create a dot map using gprof2dot
+    Command should be exactly what the normal code would be in string form
+        Ex:
+            doSomething(with, this)
+            =
+            crateDotMap("doSomething(with, this)", "test.png")
+    You can pass TMP fo
+    '''
+    # Setup Paths
+    saveOutputProfile = False
+    if outputProfile:
+        saveOutputProfile = True
+    outputProfilePath = _cleanPath(outputProfile)
+    outputProfilePath = "{0}.profile".format(os.path.splitext(outputProfilePath)[0])
+    calltreePath = os.path.splitext(outputProfilePath)[0] + '.calltree'
+    
+    # Create the profile
+    kwargs = {
+        'outputFile':outputProfilePath,
+        'cmd':cmd,
+        'frameDepth':_frameDepth,
+    }
+    result, totalTime = createProfile(**kwargs)
+
+    # Convert the profile using pyprof2calltree
+    _createPyCallGraph(outputProfilePath, calltreePath)
+
+    # Launch qcachegrind with the calltreepath
+    _launchQCacheGrind(calltreePath)
+
+    return result
+
+PYPROF2CALLTREEEXEC = 'pyprof2calltree'
+def _createPyCallGraph(profilePath, outputCallTreeFilePath):
+    cmd = "{0} -i \"{1}\" -o \"{2}\"".format(PYPROF2CALLTREEEXEC, profilePath, outputCallTreeFilePath)
+    kwargs = {}
+    if envtools.get_os() in ('mac', 'linux'):
+        kwargs['shell'] = True
+    LOG.debug("PyProf2CallTree Command: {0}".format(cmd))
+    p = production.processing.launch_subprocess(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    LOG.debug("PyProf2CallTree Command Results: {0}".format(p.communicate()))
+    LOG.debug("PyProf2CallTree return code: {0}".format(p.returncode))
+    return p.returncode  
+
+    return True
+
+QCACHEGRINDEXEC = 'qcachegrind'
+def _launchQCacheGrind(calltreePath):
+    cmd = "{0} \"{1}\"".format(QCACHEGRINDEXEC, calltreePath)
+    kwargs = {}
+    if envtools.get_os() in ('mac', 'linux'):
+        kwargs['shell'] = True
+    LOG.debug("QCacheGrind Command: {0}".format(cmd))
+    p = production.processing.launch_subprocess(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return p
 
 def createProfile(cmd, outputFile, global_dict=None, local_dict=None, frameDepth=0):
     '''
